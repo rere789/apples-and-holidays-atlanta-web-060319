d9390401326977551605601b35e8f54457bd739b
Reina Ewing <reina.castro3@gmail.com>
2019-05-22 14:42:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday|
       puts "#{season.to_s.capitalize}:" 
       holiday.each do |holiday, supply|
-        puts  " #{holiday.to_s.split(" ").each|element| element.capitalize!}.join(" ")}:#{supply.join(" ")}"
+        puts  " #{holiday.to_s.split("_").each|element| element.capitalize!}.join(" ")}:#{supply.join(" ")}"
     end 
   end 
 end
