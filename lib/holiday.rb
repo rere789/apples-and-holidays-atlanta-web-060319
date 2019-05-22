0eee853cefe7b57c4f7070e7ead3c75cff3cf990
Reina Ewing <reina.castro3@gmail.com>
2019-05-22 14:56:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
       bbq_party = []
       holiday_hash.each do |season, holiday|
         holiday.each do |event, supply|
-          bbq_party << holiday if supply.include?("BBQ")
+          bbq_party << event if supply.include?("BBQ")
           end
         end 
       bbq_party 
